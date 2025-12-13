f3880f1f750604d37db6b453c996d466a6e4ed64
Roupaswa <biswasasit14@gmail.com>
2025-12-13 16:44:45 +0530

    Update script_ver_nein.js
    Signed-off-by: Roupaswa <biswasasit14@gmail.com>

--- script_ver_nein.js
+++ script_ver_nein.js
@@ -1,17 +1,1 @@
-let solution = null;
-        // Set your secret passkey here
 
-        const PASSKEY = "OPEN";
-
-        // Check passkey input (case-sensitive)
-        function checkPasskey() {
-  const key = document.getElementById("passkeyInput").value.trim();
-
-  if (key.toUpperCase() === "OPEN") {
-    // Enable the Open Link button
-    document.getElementById("myButton").disabled = false;
-    alert("Passkey accepted! You can now open the link.");
-  } else {
-    alert("Invalid passkey. Try again.");
-  }
-}
