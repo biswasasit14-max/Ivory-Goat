61b9eb3ca0f1a7bdc4e25b06d20060056042577a
Roupaswa <biswasasit14@gmail.com>
2025-12-14 18:16:11 +0530

    Update Home.php
    Signed-off-by: Roupaswa <biswasasit14@gmail.com>

--- Home.php
+++ Home.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['authenticated'])) {
-    header("Location: index.html");
+    header("Location: access-denied.html");
     exit;
 }
 ?>
@@ -171,5 +171,6 @@ if (!isset($_SESSION['authenticated'])) {
     </footer>
 </body>
 </html>
+
 
 
