2eb0c9ca651cf8fbf95aedc3bd5471ba0240474b
Roupaswa <biswasasit14@gmail.com>
2025-12-14 12:47:03 +0530

    Update forgotPasskey.php
    Signed-off-by: Roupaswa <biswasasit14@gmail.com>

--- forgotPasskey.php
+++ forgotPasskey.php
@@ -1,14 +1,1 @@
-<?php
-// forgotPasskey.php
-session_start();
-
-// Generate a random temporary passkey
-$tempPasskey = bin2hex(random_bytes(4)); // e.g., "a3f9b2c1"
-
-// Store it with expiry (15 minutes from now)
-$_SESSION['tempPasskey'] = $tempPasskey;
-$_SESSION['tempExpiry'] = time() + (15 * 60);
-
-// Return to client
-echo $tempPasskey;
-?>
+//Do not  make anything here
