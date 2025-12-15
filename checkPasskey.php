78e01ad76b6edbc50bf31378b7f735bdc182007b
Roupaswa <biswasasit14@gmail.com>
2025-12-15 16:57:57 +0530

    Update checkPasskey.php
    Signed-off-by: Roupaswa <biswasasit14@gmail.com>

--- checkPasskey.php
+++ checkPasskey.php
@@ -5,12 +5,10 @@ session_start();
 $correctPasskey = "OPEN";
 $userPasskey = isset($_POST["passkey"]) ? trim($_POST["passkey"]) : "";
 
-if (strcasecmp($userPasskey, $correctPasskey) === 0) {
+if ($userPasskey !== "" && strcasecmp($userPasskey, $correctPasskey) === 0) {
     $_SESSION['authenticated'] = true;
     echo "success";
     exit;
-} else {
-    echo "fail";
-    exit;
 }
-?>
+echo "fail";
+exit;
